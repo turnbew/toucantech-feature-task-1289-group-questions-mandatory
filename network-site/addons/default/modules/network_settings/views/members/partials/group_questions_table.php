4928eee232622ca1152a2d31385759d1902a93de
turnbew <turnbew.co.uk@gmail.com>
2018-06-04 17:00:10 +0100

    Delete group_questions_table.php

--- network-site/addons/default/modules/network_settings/views/members/partials/group_questions_table.php
+++
@@ -1,8 +0,0 @@
-		<table class="group-questions2">	
-			<thead>
-				<?=$tableHead?>
-			</thead>
-			<tbody id="group_questions2_body_<?=$questionType?>_<?=$personTypeId?>">
-				<?=$tableBody?>
-			</tbody>
-		</table>	
